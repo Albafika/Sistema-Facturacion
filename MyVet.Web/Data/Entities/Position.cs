f554c07e1eab971bdc3521381b2cd1e63acfb80a
Cesar <blacksoul9009@hotmail.com>
2020-06-03 21:32:25 -0400

    Final

--- MyVet.Web/Data/Entities/Position.cs
+++ MyVet.Web/Data/Entities/Position.cs
@@ -12,10 +12,6 @@ namespace Sistema.Web.Data.Entities
         [Display(Name = "Código")]
         public int Position_Id { get; set; }
 
-        [Display(Name = "ID Posición")]
-        public int Codigo_Posicion { get; set; }
-
-
         [Display(Name = "Título Posición")]
         [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
